36758d9d7ff93d50f9a761582f8039e6ed3dd36c
Anushka25Sinha <111584186+Anushka25Sinha@users.noreply.github.com>
2023-06-25 00:13:36 +0530

    Time: 21 ms (29.83%), Space: 8.2 MB (30.26%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,67 +1,23 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int temp=0;
-        for(int i=0 ; i<s.size() ; i++)
+        unordered_map<char,int>mp;
+        mp['I']=1;
+        mp['V']=5;
+        mp['X']=10;
+        mp['L']=50;
+        mp['C']=100;
+        mp['D']=500;
+        mp['M']=1000;
+        
+        int ans=0;
+        for(int i=0 ;i<s.size();i++)
         {
-            if(s[i]=='I')
-            {
-                temp+=1;
-            }
-            else if(s[i]=='V')
-            {
-                temp+=5;
-            }
-             else if(s[i]=='X')
-            {
-                temp+=10;
-            }
-             else if(s[i]=='L')
-            {
-                temp+=50;
-            }
-             else if(s[i]=='C')
-            {
-                temp+=100;
-            }
-             else if(s[i]=='D')
-            {
-                temp+=500;
-            }
-             else if(s[i]=='M')
-            {
-                temp+=1000;
-            }
-           
+            if(mp[s[i]]<mp[s[i+1]])
+                ans=ans-mp[s[i]];
+            else
+                ans=ans+mp[s[i]];
         }
-         for(int i=0 ; i<s.size();i++)
-            {
-                if((s[i]=='I')&&(s[i+1]=='V'))
-                {
-                    temp=temp-2;
-                }
-                else if((s[i]=='I')&&(s[i+1]=='X'))
-                {
-                    temp=temp-2;
-                }
-                else if((s[i]=='X')&&(s[i+1]=='L'))
-                {
-                    temp=temp-20;
-                }
-                else if((s[i]=='X')&&(s[i+1]=='C'))
-                {
-                    temp=temp-20;
-                }
-                else if((s[i]=='C')&&(s[i+1]=='D'))
-                {
-                    temp=temp-200;
-                }
-                else if((s[i]=='C')&&(s[i+1]=='M'))
-                {
-                    temp=temp-200;
-                }
-
-            }
-        return temp;
+        return ans;
     }
 };
