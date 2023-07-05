717783f675177c217881b727bd9375876b12fe18
Anushka25Sinha <111584186+Anushka25Sinha@users.noreply.github.com>
2023-07-05 19:49:01 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (99.92%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        return __builtin_popcount(start^goal);
+        int count=0;
+        for(int i=32 ; i>=0 ; i--)
+        {
+            int current=(1LL<<i)&start;
+            int required=(1LL<<i)&goal;
+            if(required!=current)
+                count++;
+        }
+        return count;
     }
 };
