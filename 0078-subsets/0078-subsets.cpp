89a18024e103884690a1b1135edef856392f87f9
Anushka25Sinha <111584186+Anushka25Sinha@users.noreply.github.com>
2023-07-31 00:55:37 +0530

    Time: 4 ms (48.56%), Space: 16 MB (21.52%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,26 @@
 class Solution {
     private:
-    void subset_finder(vector<int>& nums, int index, vector<int> one_sub, vector<vector<int>> &ans)
+    void solve(vector<int>& nums, int index, vector<int> one_sub, vector<vector<int>>&ans)
     {
-        if(index>=nums.size())
+        if(index==nums.size())
         {
             ans.push_back(one_sub);
             return;
         }
+        
         one_sub.push_back(nums[index]);
-        subset_finder(nums,index+1,one_sub,ans);
+        solve(nums, index+1, one_sub, ans);
         
         one_sub.pop_back();
-        subset_finder(nums, index+1, one_sub, ans);
+        solve(nums,index+1, one_sub, ans);
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;
+     vector<vector<int>> ans;
         vector<int>one_sub;
         int index=0;
-        subset_finder(nums,index,one_sub,ans);
         
+        solve(nums,index,one_sub,ans);
         return ans;
     }
 };
