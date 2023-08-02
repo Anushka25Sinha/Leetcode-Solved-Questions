d5fcd9f7e5a4106f009c4363896dbaefb430d888
Anushka25Sinha <111584186+Anushka25Sinha@users.noreply.github.com>
2023-08-02 23:27:39 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (97.00%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,31 +1,23 @@
 class Solution {
     public:
-    void solve(vector<vector<int>>& ans, vector<int>& nums, vector<int>& ds,int freq[])
+    void solve(vector<int>& nums, vector<vector<int>>& ans,int index)
     {
-        if(ds.size()==nums.size())
+        if(index==nums.size())
         {
-            ans.push_back(ds);
+            ans.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++)
+        for(int i=index;i<nums.size();i++)
         {
-            if(freq[i]==0)
-            {
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                solve(ans,nums,ds,freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
+            swap(nums[index],nums[i]);
+            solve(nums,ans,index+1);
+            swap(nums[index],nums[i]);
         }
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        int freq[nums.size()];
-        for(int i=0;i<nums.size();i++) freq[i]=0;
-        solve(ans,nums,ds,freq);
+        solve(nums,ans,0);
         return ans;
     }
 };
